d51e62a3fd76f5c530e815ac0ca0d307df339ea1
axel <sagit117@gmail.com>
2025-03-30 08:40:35 +0300

    Цвет текста и цвет фона

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+#![crate_type = "lib"]
+#![crate_name = "cpeck_text_coloring"]
+
+pub enum Color {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Navy,
+    Purple,
+    Blue,
+    White,
+    Clear
+}
+
+pub fn unicode_escape_sequences_text(color: Color) -> String {
+    match color {
+        Color::Black => "\u{001b}[30m".to_owned(),
+        Color::Red => "\u{001b}[31m".to_owned(),
+        Color::Green => "\u{001b}[32m".to_owned(),
+        Color::Yellow => "\u{001b}[33m".to_owned(),
+        Color::Navy => "\u{001b}[34m".to_owned(),
+        Color::Purple => "\u{001b}[35m".to_owned(),
+        Color::Blue => "\u{001b}[36m".to_owned(),
+        Color::White => "\u{001b}[37m".to_owned(),
+        Color::Clear => "\u{001b}[0m".to_owned(),
+    }
+}
+
+pub fn unicode_escape_sequences_backgraund(color: Color) -> String {
+    match color {
+        Color::Black => "\u{001b}[40m".to_owned(),
+        Color::Red => "\u{001b}[41m".to_owned(),
+        Color::Green => "\u{001b}[42m".to_owned(),
+        Color::Yellow => "\u{001b}[43m".to_owned(),
+        Color::Navy => "\u{001b}[44m".to_owned(),
+        Color::Purple => "\u{001b}[45m".to_owned(),
+        Color::Blue => "\u{001b}[46m".to_owned(),
+        Color::White => "\u{001b}[47m".to_owned(),
+        Color::Clear => "\u{001b}[0m".to_owned(),
+    }
+}
